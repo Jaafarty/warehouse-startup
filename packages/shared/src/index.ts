602adf15705dc1d356000b1c2c24d90a91a3ef90
Jaafaty <jsafawi2001@gmail.com>
2026-05-01 17:39:44 +0300

    fix(shared): remove duplicate export re-declarations from index.ts

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,5 +2,3 @@ export * from "./types";
 export * from "./validation";
 export * from "./constants";
 export * from "./utils";
-export * from "./constants/pages";
-export * from "./constants/roles";
